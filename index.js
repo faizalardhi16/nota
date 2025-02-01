90734d94b00131ca4274622ecb0f991d085d7212
faizalardhi16 <74587799+faizalardhi16@users.noreply.github.com>
2025-02-01 22:00:10 +0700

    Add git

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "OK",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App listen on port 3000");
+});
